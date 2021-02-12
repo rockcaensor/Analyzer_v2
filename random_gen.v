1872f75d15daa38f509ded2ccc79ade63860be74
rockcaensor <46659725+rockcaensor@users.noreply.github.com>
2021-02-12 19:13:53 +0200

    Update random_gen.v

--- random_gen.v
+++ random_gen.v
@@ -1,4 +1,4 @@
-module random_gen
+module new_random
 #(parameter BITS = 64, INITIAL_VALUE = 64'h 5083_e3e3_8587_694b)(
 input clk, en, rs,
 output [BITS-1:0] random_data
@@ -22,7 +22,14 @@ always @(posedge clk or posedge rs)
                     begin
                         for (i = 1; i<BITS; i = i + 1)
                             begin
-                                shift_reg[0] <= ((shift_reg[3] ^ shift_reg[12]));
+                                shift_reg[0] <= ((shift_reg[3] ^ shift_reg[15]));
+                                shift_reg[15] <= ((shift_reg[32] ^ shift_reg[46]));
+                                shift_reg[46] <= ((shift_reg[57] ^ shift_reg[52]));
+                                shift_reg[52] <= ((shift_reg[54] ^ shift_reg[48]));
+                                shift_reg[48] <= ((shift_reg[16] ^ shift_reg[14]));
+                                shift_reg[14] <= ((shift_reg[47] ^ shift_reg[62]));
+                                shift_reg[62] <= ((shift_reg[35] ^ shift_reg[43]));
+                                shift_reg[43] <= ((shift_reg[38] ^ shift_reg[61]));
                                 shift_reg[i] <= shift_reg[i-1];
                             end //for
                     end  //begin
@@ -57,7 +64,14 @@ always @(negedge clk or posedge rs)
                     begin
                         for (i = 1; i<BITS; i = i + 1)
                             begin
-                                shift_reg[0] <= ((shift_reg[3] ^ shift_reg[12]));
+                                shift_reg[0] <= ((shift_reg[3] ^ shift_reg[15]));
+                                shift_reg[15] <= ((shift_reg[32] ^ shift_reg[46]));
+                                shift_reg[46] <= ((shift_reg[57] ^ shift_reg[52]));
+                                shift_reg[52] <= ((shift_reg[54] ^ shift_reg[48]));
+                                shift_reg[48] <= ((shift_reg[16] ^ shift_reg[14]));
+                                shift_reg[14] <= ((shift_reg[47] ^ shift_reg[62]));
+                                shift_reg[62] <= ((shift_reg[35] ^ shift_reg[43]));
+                                shift_reg[43] <= ((shift_reg[38] ^ shift_reg[61]));
                                 shift_reg[i] <= shift_reg[i-1];
                             end //for
                     end  //begin
